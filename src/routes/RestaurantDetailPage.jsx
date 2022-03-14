af4f4d980623fc7c41955cbf4188a8c57300bb72
kawaharm <mjbk295@gmail.com>
2022-03-13 22:05:48 -0700

    start project

---
+++ src/routes/RestaurantDetailPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function RestaurantDetailPage() {
+    return (
+        <>
+
+        </>
+    )
+}
+
+export default RestaurantDetailPage;
---
+++ src/routes/UpdatePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function UpdatePage() {
+    return (
+        <>
+
+        </>
+    )
+}
+
+export default UpdatePage;
